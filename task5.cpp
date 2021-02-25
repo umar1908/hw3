046a0ec45df8b7c867296181f97f634f81a2060c
umar1908 <umar190801@gmail.com>
2021-02-25 21:03:24 +0300

    finishing task5

--- task5.cpp
+++ task5.cpp
@@ -29,13 +29,25 @@ struct Phone_Node{
   struct HashedByNumber{};
   struct HashedBySurname{};
   struct RandomAccess{};
+  void print()const {
+    cout<<"*******************\n"<<"Name:"<<name<<endl<<"Surname:"<<surname<<"\nNumber:"<<number<<endl<<"********************\n\n";
+  }
 };
 
 using Phonebook = boost::multi_index_container<Phone_Node,
         indexed_by<
-                    ordered_unique<tag< Phone_Node::OrderedBySurname>, member<Phone_Node, string, &Phone_Node::surname> >,
-                    hashed_non_unique<tag< Phone_Node::HashedByNumber>, member<Phone_Node, long long int, &Phone_Node::number> >,
-                    random_access<>
+                    ordered_unique<
+                      tag<Phone_Node::OrderedBySurname>, member<Phone_Node, string, &Phone_Node::surname>
+                      >,
+                    hashed_non_unique<
+                      tag<Phone_Node::HashedByNumber>, member<Phone_Node, long long int, &Phone_Node::number>
+                      >,
+                      hashed_non_unique<
+                        tag<Phone_Node::HashedBySurname>, member<Phone_Node, string, &Phone_Node::surname>
+                        >,
+                    random_access<
+                      tag<Phone_Node::RandomAccess>
+                      >
                   >
         >;
 
@@ -46,15 +58,37 @@ int main(){
   cout<<endl;
 
   Phonebook book;
-  book.insert({"Umar","Makhsudov",79853635441});
-  book.insert({"Vasya","Pupkin",79998101908});
-  book.insert({"Dmitriy","Petrov",73454565677});
-  book.insert({"Andrey","Sorokin",78315010101});
+  book.insert({"Umar","B_Makhsudov",79853635441});
+  book.insert({"Vasya","A_Pupkin",79998101908});
+  book.insert({"Dmitriy","D_Petrov",73454565677});
+  book.insert({"Andrey","C_Sorokin",78315010101});
 
-  auto Ordered_Surname_index=book.get<OrderedBySurname>();
-  auto Hashed_Surname_index=book.get<HashedBySurname>();
-  auto Hashed_Number_index=book.get<HashedByNumber>();
-  auto Random_index=bool.get<RandomAccess>();
+  const auto& Ordered_Surname_index=book.get<Phone_Node::OrderedBySurname>();
+  const auto&  Hashed_Surname_index=book.get<Phone_Node::HashedBySurname>();
+  const auto& Hashed_Number_index=book.get<Phone_Node::HashedByNumber>();
+  const auto& Random_index=book.get<Phone_Node::RandomAccess>();
+
+cout<<"Доступ по индексу в цикле"<<endl;
+  for(auto elem: Random_index){ //вывод всех элементов по индексу
+  elem.print();
+}
+cout<<"Произвольный индекс по индексу"<<endl;
+Random_index[0].print(); //доступ к отдельным элементам по индексу
+Random_index[1].print();
+Random_index[2].print();
+
+
+cout<<"Доступ по индексу сортированному по фамилии в цикле"<<endl;
+for(auto elem: Ordered_Surname_index){
+  elem.print();
+}
+
+cout<<"Доступ к элементу по хэшу"<<endl;
+Hashed_Number_index.find(79853635441)->print();
+Hashed_Surname_index.find("D_Petrov")->print();
+
+
+
 
 
 
